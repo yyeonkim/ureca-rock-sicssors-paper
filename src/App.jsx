a845e6ac2e7d2caca8348eb8df058ab32e0583ac
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-10 10:11:34 +0900

    style: 카드 컴포넌트 스타일 정의

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
+import Card from "./components/Card.jsx";
+import styles from "./css/App.module.css";
+
 function App() {
   return (
     <>
       <header>
         <h1>가위바위보 게임</h1>
       </header>
-      <main className="mw">
-        <div>
-          <h1>너님</h1>
-          <img src="" alt="" />
-          <p>선택하세요(결과)</p>
-        </div>
+      <main className={styles.mw}>
+        <Card />
         <section>
           <div>버튼들</div>
           <p>결과</p>
           <button>다시하기</button>
         </section>
-        <div>컴퓨터 카드</div>
+        <Card />
       </main>
       <footer>
         <p>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import questionMark from "../assets/questionmark.png";
+import styles from "../css/Card.module.css";
 
 function Card() {
-  return <div>Card</div>;
+  return (
+    <div className={styles.card}>
+      <h1>너님</h1>
+      <img src={questionMark} alt="" />
+      <p>선택하세요</p>
+    </div>
+  );
 }
 
 export default Card;
