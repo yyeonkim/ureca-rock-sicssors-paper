1e4ebc25320d5e1dff98e56143996fd7ed1d616b
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-10 10:48:40 +0900

    feat: 카드에 type props 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import paperImg from "../assets/paper.png";
 import questionMark from "../assets/questionmark.png";
+import rockImg from "../assets/rock.png";
+import sicssorsImg from "../assets/scissors.png";
 import styles from "../css/Card.module.css";
 
-function Card() {
+const choice = {
+  ROCK: { src: rockImg, alt: "주먹" },
+  SICSSORS: { src: sicssorsImg, alt: "가위" },
+  PAPAER: { src: paperImg, alt: "보" },
+};
+
+function Card({ type }) {
   return (
     <div className={styles.card}>
       <h1>너님</h1>
-      <img src={questionMark} alt="" />
+      <img src={type ? choice[type].src : questionMark} alt={type ? choice[type].alt : "미정"} />
       <p>선택하세요</p>
     </div>
   );
