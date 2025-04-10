a66acf74de567f7c60540e37d586fa8e07c23175
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-10 14:27:58 +0900

    style: 이겼을 때 애니메이션 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,12 @@ import styles from "../css/Card.module.css";
 import { choiceImgs, results } from "../utils/constant.js";
 
 function Card({ choice, type = "user", result }) {
+  const className = `${styles.card} ${type === "computer" && styles.computer} ${
+    result === results.LOSE && styles.lose
+  }  ${result === results.WIN && styles.win}`;
+
   return (
-    <div
-      className={`${styles.card} ${type === "computer" && styles.computer} ${result === results.LOSE && styles.lose}`}
-    >
+    <div className={className}>
       <h1>{type === "computer" ? "상대선수" : "너님"}</h1>
       <img src={choice ? choiceImgs[choice] : questionMark} alt={choice ? choice : "미정"} />
       <p>{result ? result : "선택하세요"}</p>
